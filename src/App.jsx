97d85d2b39b1d74a09b44966036f03f0eac9f745
Oleg Fateev <fateev010405@gmail.com>
2025-07-24 00:13:43 +0300

    Вёрстка основы списка todo-list'а, без запроса к серверу и стилизации

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [todos, setTodos] = useState([{
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": true
+  }]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.app}>
+      <ul>
+        {todos.map(({ id, title, completed }) => (
+          <li key={id}>{title} - <span className={completed ? styles.completed : styles.incompleted}>{completed ? 'completed' : 'incompleted'}</span></li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default App
+export default App;
